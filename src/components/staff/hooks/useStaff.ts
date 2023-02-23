71043f47ddcbe6b810223caab93481498c043c77
leo.jaimes <leo777jaimes@gmail.com>
2023-02-22 19:34:51 -0600

    upload

--- src/components/staff/hooks/useStaff.ts
+++ src/components/staff/hooks/useStaff.ts
@@ -1,18 +1,19 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 
+import { useQuery } from '@tanstack/react-query';
 import type { Staff } from '../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
-  staff: Staff[];
+  staff?: Staff[];
   filter: string;
   setFilter: Dispatch<SetStateAction<string>>;
 }
@@ -22,7 +23,12 @@ export function useStaff(): UseStaff {
   const [filter, setFilter] = useState('all');
 
   // TODO: get data from server via useQuery
-  const staff: never[] = [];
+  const { data: staff } = useQuery({
+    queryKey: ['staff'],
+    queryFn: getStaff,
+  });
+
+  // const staff: never[] = [];
 
   return { staff, filter, setFilter };
 }
