e87203d729461907aa5a3aacf8bbbe61fe180341
leo.jaimes <leo777jaimes@gmail.com>
2023-02-22 19:19:30 -0600

    upload

--- src/components/treatments/hooks/useTreatments.ts
+++ src/components/treatments/hooks/useTreatments.ts
@@ -1,19 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 
-import type { Treatment } from "../../../../../shared/types";
-import { axiosInstance } from "../../../axiosInstance";
-import { queryKeys } from "../../../react-query/constants";
-import { useCustomToast } from "../../app/hooks/useCustomToast";
+import type { Treatment } from '../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const fallback = [];
-  const { data = fallback } = await axiosInstance.get("/treatments");
+  const fallback: never[] = [];
+  const { data = fallback } = await axiosInstance.get('/treatments');
 
   return data;
 }
 
-export function useTreatments(): Treatment[] {
+export function useTreatments(): Treatment[] | undefined {
   // TODO: get data from server via useQuery
   const toast = useCustomToast();
   const { data } = useQuery({
@@ -23,10 +23,10 @@ export function useTreatments(): Treatment[] {
       const title =
         error instanceof Error
           ? error.message
-          : "Error connection to the server";
+          : 'Error connection to the server';
       toast({
         title,
-        status: "error",
+        status: 'error',
       });
     },
   });
