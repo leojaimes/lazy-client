9fc7a956b1c2e42bd8faaf643d225b5b12e42d4f
leo.jaimes <leo777jaimes@gmail.com>
2023-02-24 12:16:29 -0600

    upload

--- src/components/treatments/hooks/useTreatments.ts
+++ src/components/treatments/hooks/useTreatments.ts
@@ -7,30 +7,30 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const fallback: never[] = [];
-  const { data = fallback } = await axiosInstance.get('/treatments');
+ const fallback: never[] = [];
+ const { data = fallback } = await axiosInstance.get('/treatments');
 
-  return data;
+ return data;
 }
 
 export function useTreatments(): Treatment[] | undefined {
-  // TODO: get data from server via useQuery
-  // const toast = useCustomToast();
-  const { data } = useQuery({
-    queryKey: [queryKeys.treatments],
-    queryFn: () => getTreatments(),
-    // onError: (error) => {
-    //   const title =
-    //     error instanceof Error
-    //       ? error.message
-    //       : 'Error connection to the server';
-    //   toast({
-    //     title,
-    //     status: 'error',
-    //   });
-    // },
-    // Se remouve la funcion error: onError ya que está siendo enviada desde la constante queryClient de react-query > queryCliente
-    // la variable queryClient está siendo importada en App.tsx de components > app > App.tsx
-  });
-  return data;
+ // TODO: get data from server via useQuery
+ // const toast = useCustomToast();
+ const { data } = useQuery({
+  queryKey: [queryKeys.treatments],
+  queryFn: () => getTreatments(),
+  // onError: (error) => {
+  //   const title =
+  //     error instanceof Error
+  //       ? error.message
+  //       : 'Error connection to the server';
+  //   toast({
+  //     title,
+  //     status: 'error',
+  //   });
+  // },
+  // Se remouve la funcion error: onError ya que está siendo enviada desde la constante queryClient de react-query > queryCliente
+  // la variable queryClient está siendo importada en App.tsx de components > app > App.tsx
+ });
+ return data;
 }
