1220e7461d54a616cb2b26714241e8addd60fbf5
leo.jaimes <leo777jaimes@gmail.com>
2023-02-25 11:57:42 -0600

    upload

--- src/components/staff/hooks/useStaff.ts
+++ src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 import { useQuery } from '@tanstack/react-query';
 import type { Staff } from '../../../shared/types';
@@ -8,27 +8,33 @@ import { filterByTreatment } from '../utils';
 
 // for when we need a query function for useQuery
 async function getStaff(): Promise<Staff[]> {
- const { data } = await axiosInstance.get('/staff');
- return data;
+	const { data } = await axiosInstance.get('/staff');
+	return data;
 }
 
 interface UseStaff {
- staff?: Staff[];
- filter: string;
- setFilter: Dispatch<SetStateAction<string>>;
+	staff?: Staff[];
+	filter: string;
+	setFilter: Dispatch<SetStateAction<string>>;
 }
 
 export function useStaff(): UseStaff {
- // for filtering staff by treatment
- const [filter, setFilter] = useState('all');
+	// for filtering staff by treatment
+	const [filter, setFilter] = useState('all');
 
- // TODO: get data from server via useQuery
- const { data: staff } = useQuery({
-  queryKey: [queryKeys.staff],
-  queryFn: getStaff,
- });
+	const selectFn = useCallback(
+		(data: Staff[]) => filterByTreatment(data, filter),
+		[filter]
+	);
 
- // const staff: never[] = [];
+	// TODO: get data from server via useQuery
+	const { data: staff } = useQuery({
+		queryKey: [queryKeys.staff],
+		queryFn: getStaff,
+		select: filter !== 'all' ? selectFn : undefined,
+	});
 
- return { staff, filter, setFilter };
+	// const staff: never[] = [];
+
+	return { staff, filter, setFilter };
 }
--- src/components/staff/utils.ts
+++ src/components/staff/utils.ts
@@ -1,12 +1,12 @@
-import type { Staff } from '../../../../shared/types';
+import type { Staff } from '../../shared/types';
 
 export function filterByTreatment(
-  staff: Staff[],
-  treatmentName: string,
+	staff: Staff[],
+	treatmentName: string
 ): Staff[] {
-  return staff.filter((person) =>
-    person.treatmentNames
-      .map((t) => t.toLowerCase())
-      .includes(treatmentName.toLowerCase()),
-  );
+	return staff.filter((person) =>
+		person.treatmentNames
+			.map((t) => t.toLowerCase())
+			.includes(treatmentName.toLowerCase())
+	);
 }
